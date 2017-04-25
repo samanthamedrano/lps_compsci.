5413ceaf6c1906453c430f5ee1256dde64271d0c
Samantha Medrano <rc.medrano.samantha@leadps.org>
2017-04-25 15:56:13 +0000

    Prime number list.

--- problem_sets/primeNum.py
+++ problem_sets/primeNum.py
@@ -2,23 +2,24 @@
 def Prime(ugh, list):
         wow = range(1,int(ugh))
         num = ugh - 2
-        o = 0
+        y = 0
         for number in wow:
                 yeah = int(ugh) % int(number)
 #Checks if it is a prime number.
                 if yeah != 0:   
-                        o = o + 1
-# remainder must not equal to 0 to continue.
-                        if o == num:
+                        y = y + 1
+# remainder must  equal to 0 to continue.
+                        if y == num:
                                 print(ugh)
                                 list.append(ugh)
  
 #creats a empty list
 primosList = []
 
-# ranges from 2 to 100,001.
+# ranges from 2 to 10,001, because it covers from start to end 
 rangeList = range(2, 10001)
 
+#print out the prime numbers.
 for stop in rangeList:
         gg = Prime(stop, primosList)
 
